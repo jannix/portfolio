c5fc9c115d72932b7a2fe1f3c4f33b6c27636b22
Jannix <jannix.martinez@gmail.com>
2021-04-09 15:14:46 +0200

    re create & clean project (no gatsby)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+          <h1>PIERRE JANICK MARTINEZ</h1>
+          <section>
+              <h2>FULLSTACK WEBDEV</h2>
+              <p>Que vous souhaitiez simplement augmenter la visibilité de votre activité ou bien envoyer votre
+                  site web sur Mars, je peux vous aider à relever vos challenges.</p>
+              <p>Je crée des solutions web à l 'aide des frameworks les plus pertinents pour tous vos types de projet. Je
+                  développe la partie front selon vos recommandations artistiques et la partie back avec un soucis de
+                  performance et de stabilité.</p>
+          </section>
+          <section>{/*Here will be exp component*/}
+          </section>
+          <div>
+              <section>{/*Here will be edu component*/}
+              </section>
+              <section>{/*Here will be skills component*/}
+              </section>
+          </div>
+          <span> M A R T I N E Z P I E R R E . J [at] G M A I L . C O M</span>
+      </main>
     </div>
   );
 }
